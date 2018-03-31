43af16ac1852477f6fedc52fb34a4e7b41fcea3a
Unknown <Atracks@bk.ru>
2018-04-01 00:21:19 +0500

    Model validator id range fix

--- src/main/java/ru/bravery_and_stupidity/podcast_parser/model/validator/ModelValidator.java
+++ src/main/java/ru/bravery_and_stupidity/podcast_parser/model/validator/ModelValidator.java
@@ -14,14 +14,14 @@ public class ModelValidator {
 
     public static void validate(@Nullable Category item) throws IllegalStateException, IllegalArgumentException {
         Assert.notNull(item, NOT_NULL_MSG);
-        Assert.state(item.getId() > 0, NEGATIVE_ID);
+        Assert.state(item.getId() >= 0, NEGATIVE_ID);
         Assert.hasLength(item.getName().trim(), NOT_EMPTY_MSG);
         Assert.hasLength(item.getUrl().trim(), NOT_EMPTY_MSG);
     }
 
     public static void validate(@Nullable Podcast item) throws IllegalStateException, IllegalArgumentException {
         Assert.notNull(item, NOT_NULL_MSG);
-        Assert.state(item.getId() > 0, NEGATIVE_ID);
+        Assert.state(item.getId() >= 0, NEGATIVE_ID);
         Assert.state(item.getCategoryId() > 0, NEGATIVE_ID);
         Assert.hasLength(item.getName().trim(), NOT_EMPTY_MSG);
         Assert.hasLength(item.getUrl().trim(), NOT_EMPTY_MSG);
